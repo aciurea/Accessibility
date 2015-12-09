d62daccf2e745161d96cc4c60f0c9c11babe0471
aciurea <alinciurea90@gmail.com>
2015-12-09 13:33:31 +0200

    Added the dll files along with angular libraries

---
+++ Accessibility/Controllers/AccessibilityController.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Accessibility.Controllers
+{
+    public class AccessibilityController : Controller
+    {
+        // GET: Accessibility
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: Accessibility/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Accessibility/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Accessibility/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Accessibility/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Accessibility/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Accessibility/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Accessibility/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ Accessibility/Scripts/app/app.js
@@ -0,0 +1,6 @@
+﻿(function () {
+    var app = angular.module('app', []);
+    app.controller('MainController', function ($scope) {
+        $scope.test = "test";
+    });
+}());
